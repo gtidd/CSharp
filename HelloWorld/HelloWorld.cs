059176696613ab9fb89ad70a34312c444bb49716
gtidd <gregtiddhomes@gmail.com>
2016-07-27 14:57:32 -0600

    Uploading HelloWorld app

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,13 @@
+using System; //namespace 
+
+
+public class Program
+{
+	static void Main()
+	{
+		Console.Writeline("Hello, World!");
+		Console.Writeline(DateTime.Now.DayOfWeek);
+		Console.WriteLine("Your Name:");
+		string name = Console.ReadLine();
+	}
+}
